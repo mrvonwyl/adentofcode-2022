1a562fa76c2dd434ad47bf69b4f7dd5f32768f1e
Marcel von Wyl <mrvonwyl@gmail.com>
2022-12-01 11:40:58 +0100

    change way of invoking daily challenges

--- src/1.ts
+++ src/1.ts
@@ -1,20 +1,16 @@
 import { data } from './1.data';
 
-export function fn1_1(): void {
-  const result = data
+export function fn1_1(): number {
+  return data
     .split('\n\n')
-    .map((elveCalories) =>
-      elveCalories.split('\n').reduce((a, b) => +a + +b, 0)
-    )
+    .map((c) => c.split('\n').reduce((a, b) => +a + +b, 0))
     .reduce((a, b) => {
       return Math.max(a, b);
     });
-
-  console.log('1-1', result);
 }
 
-export function fn1_2(): void {
-  const result = data
+export function fn1_2(): number {
+  return data
     .split('\n\n')
     .map((elveCalories) =>
       elveCalories.split('\n').reduce((a, b) => +a + +b, 0)
@@ -22,6 +18,4 @@ export function fn1_2(): void {
     .sort((a, b) => b - a)
     .slice(0, 3)
     .reduce((a, b) => a + b, 0);
-
-  console.log('1-2:', result);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { fn1_1, fn1_2 } from './1';
+import * as day1 from './1';
 
-fn1_1();
-fn1_2();
+Object.values(day1).forEach((f, index) => {
+  const result = f();
+  console.log(`Day 1, Challenge ${index + 1}:`, result);
+});
