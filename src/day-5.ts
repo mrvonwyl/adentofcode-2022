1ab46ba66fb4a93818210cac98015cddb867ce2f
Marcel von Wyl <mrvonwyl@gmail.com>
2022-12-05 15:38:34 +0100

    day 5 challenge 2

--- src/day-5.ts
+++ src/day-5.ts
@@ -36,7 +36,7 @@ export function challenge1(): string {
       return [+matches[1], +matches[2], +matches[3]];
     })
     .reduce((sta, [amount, from, to]) => {
-      console.log('Stacks', sta);
+      // console.log('Stacks', sta);
 
       for (
         let colStack = 0;
@@ -58,25 +58,67 @@ export function challenge1(): string {
     .reduce((a, b) => a.concat(b), '');
 }
 
-// export function challenge2(): number {
-//   return data
-//     .split('\n')
-//     .map((pair) =>
-//       pair
-//         .split(',')
-//         .map((sections) =>
-//           sections.split('-').map((a) => +a)
-//         )
-//     )
-//     .filter(([[s1, e1], [s2, e2]]) => {
-//       const a = e1 >= s2 && s1 <= s2;
-//       const b = e2 >= s1 && s2 <= s1;
+export function challenge2(): string {
+  const stacks: string[][] = [];
 
-//       return a || b;
+  const containerRows = data1
+    .split('\n')
+    .map((line) => `${line} `)
+    .filter((line) => !line.includes('1'))
+    .map((line) => {
+      return line
+        .match(/.{1,4}/g)
+        ?.map((container) => container.substring(1, 2));
+    });
 
-//       // const firstContainsSecond = s1 <= s2 && e1 >= e2;
-//       // const secondContainsFirst = s2 <= s1 && e2 >= e1;
+  containerRows.forEach((containerRow, rowIndex) => {
+    containerRow?.forEach((container, colIndex) => {
+      if (container !== ' ') {
+        if (!stacks[colIndex]) {
+          stacks[colIndex] = [];
+        }
 
-//       // return firstContainsSecond || secondContainsFirst;
-//     }).length;
-// }
+        stacks[colIndex].unshift(container);
+      }
+    });
+  });
+
+  console.log('Stacks', stacks);
+
+  const manipulatedStacks = data2
+    .split('\n')
+    .map((instructions) => {
+      const matches =
+        instructions.match(
+          /move\s(\d*)\sfrom\s(\d*)\sto\s(\d*)/
+        ) ?? [];
+
+      return [+matches[1], +matches[2], +matches[3]];
+    })
+    .reduce((sta, [amount, from, to]) => {
+      console.log(`move ${amount} from ${from} to ${to}`);
+
+      if (amount > 1) {
+        console.log('Move many', amount);
+
+        sta[to - 1].push(
+          ...sta[from - 1].splice(amount * -1)
+        );
+      } else {
+        console.log('Move one', amount);
+        sta[to - 1].push(sta[from - 1].pop());
+      }
+
+      console.log(stacks);
+      console.log('_________________');
+
+      return sta;
+    }, stacks);
+
+  // console.log(containerRows);
+  // console.log(manipulatedStacks);
+
+  return manipulatedStacks
+    .map((col) => col.pop())
+    .reduce((a, b) => a.concat(b), '');
+}
