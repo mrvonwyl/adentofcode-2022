b397481a5f860fefb303452a2ac9a45a8f4f8491
Marcel von Wyl <mrvonwyl@gmail.com>
2022-12-05 08:16:00 +0100

    day 4

--- src/day-4.ts
+++ src/day-4.ts
@@ -1,7 +1,42 @@
+import { data } from './day-4.data';
+
 export function challenge1(): number {
-  return 0;
+  return data
+    .split('\n')
+    .map((pair) =>
+      pair
+        .split(',')
+        .map((sections) =>
+          sections.split('-').map((a) => +a)
+        )
+    )
+    .filter(([[s1, e1], [s2, e2]]) => {
+      const firstContainsSecond = s1 <= s2 && e1 >= e2;
+      const secondContainsFirst = s2 <= s1 && e2 >= e1;
+
+      return firstContainsSecond || secondContainsFirst;
+    }).length;
 }
 
 export function challenge2(): number {
-  return 0;
+  return data
+    .split('\n')
+    .map((pair) =>
+      pair
+        .split(',')
+        .map((sections) =>
+          sections.split('-').map((a) => +a)
+        )
+    )
+    .filter(([[s1, e1], [s2, e2]]) => {
+      const a = e1 >= s2 && s1 <= s2;
+      const b = e2 >= s1 && s2 <= s1;
+
+      return a || b;
+
+      // const firstContainsSecond = s1 <= s2 && e1 >= e2;
+      // const secondContainsFirst = s2 <= s1 && e2 >= e1;
+
+      // return firstContainsSecond || secondContainsFirst;
+    }).length;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const dayToExecute = new Date().getDate();
 
-import(`./day-3`).then(
+import(`./day-4`).then(
   (imports) => {
     Object.values(imports)
       .map((fn) => fn as () => unknown)
